5c9ca9817c681e23879a0b694688ef103451dac9
Nasser Collado <nasserisraelcollado@gmail.com>
2026-09-06 18:02:24 -0400

    modificacion

--- src/pages/carta.tsx
+++ src/pages/carta.tsx
@@ -5,20 +5,15 @@ export default function CartaPage() {
   return (
     <AnimatedLoveBackground>
       <Carta
-        texto={`Mi amor,
+        texto={`Rosangel mi amor,
           hoy como siempre estoy pensando en ti y en lo afortunado que soy de tenerte en mi vida.
-          No tengo palabras para lo arrepentido que me siento por todo lo que te he hecho sentir, 
-          pero quiero que sepas que estoy dispuesto a hacer todo lo posible para enmendar mis errores y demostrarte cuánto te amo.
+          No tengo palabras para lo arrepentido que me siento por todo lo que te he hecho sentir, pero quiero que sepas que estoy dispuesto a hacer todo lo posible para enmendar mis errores y demostrarte cuánto te amo.
           Eres la persona mas importante en mi vida y la mujer que hace que cada día valga la pena.
-          Yo aunque te ame, te quiera y te adore no soy perfecto y me cuesta darme cuenta de mis errores, y de como te hago sentir,
-          por que se que te he lastimado y no quiero que eso vuelva a pasar, quiero que me ayudes a ser mejor persona y a ser el hombre que te mereces,
-          quiero que me ayudes a ser el hombre que te haga feliz y que te haga sentir amada.
-          Te amo con todo mi corazón y quiero pasar el resto de mi vida contigo, quiero que sepas que siempre estaré aquí para ti, 
-          apoyándote y amándote en cada paso del camino.
+          Yo aunque te ame, te quiera y te adore no soy perfecto y me cuesta darme cuenta de mis errores, y de como te hago sentir,por que se que te he lastimado y no quiero que eso vuelva a pasar, quiero que me ayudes a ser mejor persona y a ser el hombre que te mereces, quiero que me ayudes a ser el hombre que te haga feliz y que te haga sentir amada.
+          Te amo con todo mi corazón y quiero pasar el resto de mi vida contigo, quiero que sepas que siempre estaré aquí para ti, apoyándote y amándote en cada paso del camino.
           Gracias por ser mi compañera, mi amiga y mi amor. 
           Te amo más de lo que las palabras pueden expresar y siempre lo haré.
-          
-          for Rosangel❤️`}
+          Att: Nasser un hombre arrepentido❤️`}
       />
     </AnimatedLoveBackground>
   );
