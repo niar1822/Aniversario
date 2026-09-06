54fcec968697a9535fbf03d410fd57c7d1032b65
Nasser Collado <nasserisraelcollado@gmail.com>
2026-09-06 17:56:37 -0400

    nueva carta

--- src/pages/carta.tsx
+++ src/pages/carta.tsx
@@ -6,14 +6,19 @@ export default function CartaPage() {
     <AnimatedLoveBackground>
       <Carta
         texto={`Mi amor,
-
-Quiero que sepas que cada momento contigo se ha convertido en uno de mis recuerdos favoritos.
-
-Gracias por estar a mi lado, por hacerme sonreír y por compartir conmigo tantos momentos hermosos,
-
-desde que te conoci, tu has sido siempre mi mayor tesoro, gracias por ser mi mejor recuerdo
-
-Te amo muchísimo Rosie ❤️`}
+          hoy como siempre estoy pensando en ti y en lo afortunado que soy de tenerte en mi vida.
+          No tengo palabras para lo arrepentido que me siento por todo lo que te he hecho sentir, 
+          pero quiero que sepas que estoy dispuesto a hacer todo lo posible para enmendar mis errores y demostrarte cuánto te amo.
+          Eres la persona mas importante en mi vida y la mujer que hace que cada día valga la pena.
+          Yo aunque te ame, te quiera y te adore no soy perfecto y me cuesta darme cuenta de mis errores, y de como te hago sentir,
+          por que se que te he lastimado y no quiero que eso vuelva a pasar, quiero que me ayudes a ser mejor persona y a ser el hombre que te mereces,
+          quiero que me ayudes a ser el hombre que te haga feliz y que te haga sentir amada.
+          Te amo con todo mi corazón y quiero pasar el resto de mi vida contigo, quiero que sepas que siempre estaré aquí para ti, 
+          apoyándote y amándote en cada paso del camino.
+          Gracias por ser mi compañera, mi amiga y mi amor. 
+          Te amo más de lo que las palabras pueden expresar y siempre lo haré.
+          
+          for Rosangel❤️`}
       />
     </AnimatedLoveBackground>
   );
