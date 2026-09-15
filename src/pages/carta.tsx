51818b355268b6054d7703717242d942f097831a
Nasser Collado <nasserisraelcollado@gmail.com>
2026-09-15 01:40:06 -0400

    ortografia

--- src/pages/carta.tsx
+++ src/pages/carta.tsx
@@ -9,7 +9,7 @@ export default function CartaPage() {
           hoy como todos los dias vengo aqui a espresarte lo que mis acciones no pueden decirte, y es que te amo con todo mi corazon, 
           eres la mujer mas hermosa del mundo y la que me hace sentir el hombre mas feliz del mundo, gracias por estar a mi lado y por hacerme sentir amado, eres mi todo y siempre lo seras,
           te amo con todo mi corazon y siempre lo hare, eres esa hermosa flor que nunca se marchita, y el sol de las mañanas ese que me despierta con su gran brillo,
-          te amo mas de lo que las palabras pueban expresar, y mucho mas de lo que mi corazon puede sentir, eres mi todo y siempre lo seras, gracias por ser mi novia y por hacerme sentir amado,
+          te amo mas de lo que las palabras puedan expresar, y mucho mas de lo que mi corazon puede sentir, eres mi todo y siempre lo seras, gracias por ser mi novia y por hacerme sentir amado,
           te amo.
           Amor tu novio❤️`}
       />
